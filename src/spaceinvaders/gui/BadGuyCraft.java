0ec97fad437b033b71db1dc5cd88faa021058f67
Chris Cannon <chris.c.canon@gmail.com>
2018-05-13 18:18:51 -0400

    Add additional todo instructions

--- src/spaceinvaders/gui/BadGuyCraft.java
+++ src/spaceinvaders/gui/BadGuyCraft.java
@@ -1,10 +1,9 @@
 package spaceinvaders.gui;
 
-public class BadGuyCraft extends Craft {
+//TODO: make BadGuyCraft a subclass of Craft
+public class BadGuyCraft {
     public BadGuyCraft(String imageFilePath, double startingX, double startingY, double speed){
-        super(imageFilePath, startingX, startingY);
-        this.setDirection(0.0);
-        this.setSpeed(speed);
-        this.setHitPoints(10);
+        //TODO: pass the first three arguments of this constructor to the constructor of the super class
+        //TODO: use the set methods of craft to initialize direction to 0, hit points to 10, and speed to the given speed
     }
 }
